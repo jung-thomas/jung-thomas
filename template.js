ecb0d59cb156a0a766540c21f1782dc6c5f167eb
Thomas Jung <thomas.jung@sap.com>
2020-07-21 17:24:34 -0400

    Better layout

--- template.js
+++ template.js
@@ -8,7 +8,7 @@ I'm currently Head of [SAP Developer Advocacy](https://developers.sap.com/) team
 ## On my blog
 {{#scnCont}}- [{{{displayName}}}]({{{url}}}) ({{date}})
 {{/scnCont}} 
-More on [My SAP Community Blog](https://people.sap.com/thomas.jung#content:blogposts)
+- More on [My SAP Community Blog](https://people.sap.com/thomas.jung#content:blogposts)
 </td>
   
 <td valign="top" width="50%">
@@ -16,7 +16,7 @@ More on [My SAP Community Blog](https://people.sap.com/thomas.jung#content:blogp
 ## Videos and Live Streams
 {{#items}}- [{{{title}}}]({{{link}}}) ({{date}})
 {{/items}}
-More on [My YouTube Channel](https://www.youtube.com/channel/UCVLSTvSR7UAd87o_0qoIR4Q)
+- More on [My YouTube Channel](https://www.youtube.com/channel/UCVLSTvSR7UAd87o_0qoIR4Q)
 </td></tr></table>
 
 Follow me on [Twitter](https://twitter.com/thomas_jung) or [LinkedIn](https://www.linkedin.com/in/thomasjungsap/).
