e5a9591ed8276240d2bb41622ccf377e2fbdc56f
Thomas Jung <thomas.jung@sap.com>
2020-07-21 17:14:31 -0400

    Initial commit with dynamic content

--- template.js
+++ template.js
@@ -1,19 +1,24 @@
+module.exports = `
 # Hi there 👋
 
 I'm currently Head of [SAP Developer Advocacy](https://developers.sap.com/) team at @SAP .
 
 <table><tr><td valign="top" width="50%">
-
+ 
 ## On my blog
-
+{{#scnCont}}- [{{{displayName}}}]({{{url}}}) ({{date}})
+{{/scnCont}} 
 More on [My SAP Community Blog](https://people.sap.com/thomas.jung#content:blogposts)
 </td>
   
 <td valign="top" width="50%">
   
 ## Videos and Live Streams
-
+{{#items}}- [{{{title}}}]({{{link}}}) ({{date}})
+{{/items}}
 More on [My YouTube Channel](https://www.youtube.com/channel/UCVLSTvSR7UAd87o_0qoIR4Q)
 </td></tr></table>
 
 Follow me on [Twitter](https://twitter.com/thomas_jung) or [LinkedIn](https://www.linkedin.com/in/thomasjungsap/).
+
+`
