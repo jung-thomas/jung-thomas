ca18f9b9bbc83b41171e56112f75564874ddc09d
Thomas Jung <thomas.jung@sap.com>
2021-06-14 15:19:26 -0400

    Add SAP Community Content

--- template.js
+++ template.js
@@ -3,10 +3,9 @@ module.exports = `
 
 I'm currently Head of [SAP Developer Advocacy](https://developers.sap.com/developer-advocates.html) team at SAP .
 
-<p align="center"> 
 [![Visits Badge](https://badges.pufler.dev/visits/jung-thomas/jung-thomas)](https://badges.pufler.dev)
+[![Updated Badge](https://badges.pufler.dev/updated/puf17640/git-badges)](https://badges.pufler.dev)
 [![Commits Badge](https://badges.pufler.dev/commits/monthly/jung-thomas)](https://badges.pufler.dev)
-</p>
 
 <table><tr><td valign="top" width="50%">
  
@@ -24,17 +23,39 @@ I'm currently Head of [SAP Developer Advocacy](https://developers.sap.com/develo
 - More on [SAP Developers YouTube Channel](https://www.youtube.com/channel/UCNfmelKDrvRmjYwSi9yvrMg)
 </td></tr></table>
 
-Follow me on [Twitter](https://twitter.com/thomas_jung) or [LinkedIn](https://www.linkedin.com/in/thomasjungsap/).
+Follow me on <a href="https://twitter.com/thomas_jung"><img alt="Twitter" src="https://img.shields.io/badge/thomas_jung-%231DA1F2.svg?style=for-the-badge&logo=Twitter&logoColor=white"/></a> or <a href="https://www.linkedin.com/in/thomasjungsap/"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white"/></a>.
 
 ![README builder](https://github.com/jung-thomas/jung-thomas/workflows/README%20builder/badge.svg)
 
+## SAP Community Activity
+<p align = "center">
+<a href="https://people.sap.com/thomas.jung#overview">
+  <img align="center" src="https://devrel-tools-prod-scn-badges-srv.cfapps.eu10.hana.ondemand.com/activity/thomas.jung" />
+</a>
+<a href="https://people.sap.com/thomas.jung#reputation">
+  <img align="center" src="https://devrel-tools-prod-scn-badges-srv.cfapps.eu10.hana.ondemand.com/showcaseBadges/thomas.jung" />
+</a>
+</p>
+
+## GitHub Statistics
 <p align = "center">
 <a href="https://github.com/anuraghazra/github-readme-stats">
-  <img align="center" src="https://github-readme-stats.vercel.app/api?username=jung-thomas&count_private=true&show_icons=true&theme=dark" />
+  <img align="center" src="https://github-readme-stats.vercel.app/api?username=jung-thomas&count_private=true&show_icons=true&theme=dark&line_height=27" />
 </a>
 <a href="https://github.com/anuraghazra/github-readme-stats">
   <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=jung-thomas&show_icons=true&theme=dark" />
 </a>
 </p>
+
+<p align = "center">
+ <img  src="https://github-readme-streak-stats.herokuapp.com/?user=jung-thomas&show_icons=true&locale=en&layout=compact&theme=dark&line_height=0" />
+</p> 
+
+<p align = "center">
+ <img src="https://activity-graph.herokuapp.com/graph?username=jung-thomas&theme=redical">
+</p> 
+
+[![trophy](https://github-profile-trophy.vercel.app/?username=jung-thomas&theme=onedark)](https://github.com/ryo-ma/github-profile-trophy)
+
 `
 
