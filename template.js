ed275b4919fab97234a6330b95b67ea0872d3304
Thomas Jung <thomas.jung@sap.com>
2026-04-08 18:20:34 -0300

    rewrite template: remove broken widgets, add about/tech-stack/featured-projects sections

--- template.js
+++ template.js
@@ -1,71 +1,66 @@
 module.exports = `
 # Hi there 👋🏼
 
-I'm currently Head of [SAP Developer Advocacy](https://developers.sap.com/developer-advocates.html) team at SAP .
+I'm **Thomas Jung**, Head of [SAP Developer Advocacy](https://developers.sap.com/developer-advocates.html) at SAP.
 
-[![Open in Visual Studio Code](https://img.shields.io/badge/Made%20for-VSCode-1f425f.svg)](https://github.dev/jung-thomas/jung-thomas)
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=flat&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/thomasjungsap/)
 
-Follow me on:
-- <a href="https://twitter.com/thomas_jung"><img alt="Twitter" src="https://img.shields.io/badge/thomas_jung-%231DA1F2.svg?style=for-the-badge&logo=Twitter&logoColor=white"/></a>
-- <a href="https://www.linkedin.com/in/thomasjungsap/"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white"/></a>
-- <a rel="me" href="https://mastodon.cloud/@thomas_jung"><img alt="Mastodon" src="https://img.shields.io/mastodon/follow/109262551990174478?domain=https%3A%2F%2Fmastodon.cloud%2F&style=social"/></a>
+---
 
-![README builder](https://github.com/jung-thomas/jung-thomas/workflows/README%20builder/badge.svg)
+## About
+
+Nearly three decades in SAP — from ABAP developer to Head of Developer Advocacy. I lead a team that creates tutorials, videos, sample code, and CodeJams to help developers cut through the noise and actually build things.
+
+---
 
 <table><tr><td valign="top" width="50%">
- 
-## On my blog
-{{#items}}- [{{{title}}}]({{{id}}}) 
-{{/items}} 
-- More on [My SAP Community Blog](https://community.sap.com/t5/user/viewprofilepage/user-id/139)
-</td>
-  
-<td valign="top" width="50%">
-  
-## Videos and Live Streams
+
+## Latest Videos
 {{#itemsNew}}- [{{{title}}}]({{{link}}}) ({{date}})
 {{/itemsNew}}
-- More on [SAP Developers YouTube Channel](https://www.youtube.com/channel/UCNfmelKDrvRmjYwSi9yvrMg) ![](https://img.shields.io/youtube/channel/views/UCNfmelKDrvRmjYwSi9yvrMg) ![](https://img.shields.io/youtube/channel/subscribers/UCNfmelKDrvRmjYwSi9yvrMg)
-</td></tr></table>
+- More on [SAP Developers YouTube Channel](https://www.youtube.com/channel/UCNfmelKDrvRmjYwSi9yvrMg)
+
+</td><td valign="top" width="50%">
 
 ## Upcoming SAP CodeJams
 {{#events}}- [{{{title}}}]({{{href}}})
-  - <img src="{{{thumb}}}" />
   - Start: {{{startTimeFormatted}}}
   - Location: {{{location}}}
 {{/events}}
 - More on [the SAP CodeJam Community Events Calendar](https://groups.community.sap.com/t5/sap-codejam/eb-p/codejam-events)
 
-## SAP Community Activity
-<p align = "center">
-<a href="https://community.sap.com/t5/user/viewprofilepage/user-id/139">
-  <img align="center" src="https://devrel-tools-prod-scn-badges-srv.cfapps.eu10.hana.ondemand.com/activity/139" />
-</a>
-</br>
-<a href="https://community.sap.com/t5/user/viewprofilepage/user-id/139">
-  <img align="center" src="https://devrel-tools-prod-scn-badges-srv.cfapps.eu10.hana.ondemand.com/showcaseBadges/139/1570/674/384/900/390" />
-</a>
-</p>
+</td></tr></table>
 
-## GitHub Statistics
-<p align = "center">
-<a href="https://github.com/anuraghazra/github-readme-stats">
-  <img align="center" src="https://github-readme-stats.vercel.app/api?username=jung-thomas&count_private=true&show_icons=true&theme=dark&line_height=27" />
-</a>
-<a href="https://github.com/anuraghazra/github-readme-stats">
-  <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=jung-thomas&show_icons=true&theme=dark" />
-</a>
-</p>
+---
 
-<p align = "center">
- <img  src="https://github-readme-streak-stats.herokuapp.com/?user=jung-thomas&show_icons=true&locale=en&layout=compact&theme=dark&line_height=0" />
-</p> 
+## Tech Stack
 
-<p align = "center">
- <img src="https://activity-graph.herokuapp.com/graph?username=jung-thomas&theme=redical">
-</p> 
+<img src="https://img.shields.io/badge/SAP%20BTP-0070F2?style=flat&logo=sap&logoColor=white" alt="SAP BTP" />
+<img src="https://img.shields.io/badge/CAP%20Node.js-0070F2?style=flat&logo=sap&logoColor=white" alt="CAP Node.js" />
+<img src="https://img.shields.io/badge/SAP%20HANA%20Cloud-00A4E4?style=flat&logo=sap&logoColor=white" alt="SAP HANA Cloud" />
+<img src="https://img.shields.io/badge/ABAP-0070F2?style=flat&logo=sap&logoColor=white" alt="ABAP" />
+<img src="https://img.shields.io/badge/SAP%20AI%20Core-6E40C9?style=flat&logo=sap&logoColor=white" alt="SAP AI Core" />
+<img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat&logo=javascript&logoColor=black" alt="JavaScript" />
+<img src="https://img.shields.io/badge/Joule%20Studio-E8503A?style=flat&logo=sap&logoColor=white" alt="Joule Studio" />
 
-[![trophy](https://github-profile-trophy.vercel.app/?username=jung-thomas&theme=onedark)](https://github.com/ryo-ma/github-profile-trophy)
+---
 
+## Featured Projects
+
+<table>
+{{#repoRows}}
+<tr>
+{{#this}}
+<td valign="top" width="50%">
+
+**[{{{name}}}]({{{url}}})**
+
+{{{description}}}
+
+</td>
+{{/this}}
+</tr>
+{{/repoRows}}
+</table>
 `
 
